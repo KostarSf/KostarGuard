8b1df91b6f063c647aeb664b5aff1893f4c53557
Maksim Peskov <kostarsf@gmail.com>
2021-11-08 02:33:31 +0300

    Change version placeholder

--- src/main/java/com/github/kostarsf/kostarguard/PluginDetails.java
+++ src/main/java/com/github/kostarsf/kostarguard/PluginDetails.java
@@ -5,7 +5,7 @@ public class PluginDetails {
 
     public static final String ID = "kostarguard";
     public static final String NAME = "KostarGuard";
-    public static final String VERSION = "@project.version@";
+    public static final String VERSION = "${project.version}";
     public static final String DESCRIPTION = "A simple server nickname protection for Velocity";
     public static final String URL = "https://github.com/KostarSf/KostarGuard";
     public static final String AUTHOR = "KostarSf";
