0e1de4a14e9ab0cef1c7ef0570a24dd42eb97da0
Maksim Peskov <kostarsf@gmail.com>
2021-11-08 02:01:34 +0300

    Create external plugin info class

---
+++ src/main/java/com/github/kostarsf/kostarguard/PluginDetails.java
@@ -0,0 +1,12 @@
+package com.github.kostarsf.kostarguard;
+
+public class PluginDetails {
+    private  PluginDetails() {}
+
+    public static final String ID = "kostarguard";
+    public static final String NAME = "KostarGuard";
+    public static final String VERSION = "@project.version@";
+    public static final String DESCRIPTION = "A simple server nickname protection for Velocity";
+    public static final String URL = "https://github.com/KostarSf/KostarGuard";
+    public static final String AUTHOR = "KostarSf";
+}
